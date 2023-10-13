d0484ce77246f562c55508c87ba8b351fab390d7
Margot Hilt <margot.hilt@simovative.com>
2023-10-13 09:00:19 +0200

    added php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+ echo 'Hello world';
+
+ $test4 = 23; //integer
+ $test5 = 345.5; //float
+ $test6 = true; //boolean
+ $test8 = 'Hallo'; //string
+ $test9 = []; //array
+ $test = null; //nix
+
+if (!$test6) {
+    echo ' Simovative';
+}
